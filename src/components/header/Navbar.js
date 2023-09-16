4eb83bf0f39c0ea73b76b3ab4ef4e6d973c71903
Nipun Ramani <2822656r@student.gla.ac.uk>
2023-09-16 17:44:55 +0100

    updated fonts

--- src/components/header/Navbar.js
+++ src/components/header/Navbar.js
@@ -19,14 +19,14 @@ function Navbar() {
       
       <div className='d-flex site-header py-2'>
       {/* <img src="party_hat1.webp" href="" className='mb-2 me-auto' id="party" alt="" width="35" height="35"/> */}
-      <BootstrapNavbar.Brand className="ms-auto me-auto my-2 brand-title">
+      <BootstrapNavbar.Brand className="ms-auto me-auto my-2 brand-title" id="brand-title-name">
       NIPUN RAMANI</BootstrapNavbar.Brand>
        <Nav className="ms-auto me-auto"
                     style={{maxHeight:'100px'}}>
-       <Nav.Link className="mleft-1" id="nav-link"  href="/about">About Me</Nav.Link>
-       <Nav.Link className="mleft-1" id="nav-link" href="/project">Project</Nav.Link>
-       <Nav.Link className="mleft-1" id="nav-link" href="/posts">Posts</Nav.Link>
-       <Nav.Link className="mleft-1" id="nav-link" href="/socials">Socials</Nav.Link>
+       <Nav.Link className="mleft-1" id="nav-link"  href="/about">about me</Nav.Link>
+       <Nav.Link className="mleft-1" id="nav-link" href="/project">projects</Nav.Link>
+       <Nav.Link className="mleft-1" id="nav-link" href="/posts">posts</Nav.Link>
+       <Nav.Link className="mleft-1" id="nav-link" href="/socials">socials</Nav.Link>
        
        </Nav>
         </div>
