8d9057094237f5f595d3c8ad0f5c68ba5976c29b
Nipun Ramani <2822656r@student.gla.ac.uk>
2023-09-23 17:20:48 +0100

    using cards to display projects

--- src/components/portfolio/Project.js
+++ src/components/portfolio/Project.js
@@ -1,19 +1,20 @@
 import React from "react";
 import ProjectEntry from "./ProjectEntry";
+import projectList from "../../projects.json"
 function Project() {
-  const project = {
-    preview: "/img/mach/bridge_train.gif",
-    title: "Mach",
-    text: "Mach is a physics engine made in C++. It comes with a sandbox in which user can play with physics objects.",
-    skills: ["C++", "Visual Studio", "CMake", "gTest"]
-  };
+
   return (
-    <div class="container mt-4" style={{maxWidth:"800px"}}>
+    <div class="container mt-4" style={{maxWidth:"1000px"}}>
   
     <div className="me-auto ms-auto">
     <h3>Projects</h3>
-        <ProjectEntry project={project} />
-      </div>
+    <div className="d-flex flex-wrap m-1">
+    { projectList && projectList.map((project)=>{
+       return(<ProjectEntry project={project} />);
+      })
+    }
+    </div>
+    </div>
     </div>
   );
 }
--- src/components/portfolio/ProjectEntry.js
+++ src/components/portfolio/ProjectEntry.js
@@ -1,33 +1,52 @@
-import React from 'react'
+import React from "react";
+import { Link } from "react-router-dom";
 
-const ProjectEntry = ({project}) => {
+const ProjectEntry = ({ project }) => {
   return (
-    <div class="project-entry d-md-flex flex-md-row">
-    <div class="project-header">
-      <img
-        src={project.preview}
-        class="project-img"
-        width="100%"
-        height="100%"
-      />
-    </div>
-    <div class="project-body d-flex flex-column ms-md-2">
-      <div class="project-title">{project.title}</div>
-      <div class="project-text">
-      {project.text}
+    <div className="project-card">
+      <div className="project-card-bg-image">
+      <img width="100%" height="100%" src={project.preview} style={{borderRadius:"10px"}} />
       </div>
-      <div class="project-tag">
-        {
-            project.skills.forEach((skill)=>{
-                return (
-                    <div class="skill-tag"> {skill} </div>
-                )
-            })
-        }
+      <div className="project-card-description">
+      
+      <div class="project-card-title"><Link to={project.link}>{project.title}</Link></div>
+      
+      <div class="project-card-text">{project.text}</div>
       </div>
     </div>
-  </div>
-  )
-}
+  );
+};
+
+// const ProjectEntry = ({project}) => {
+//   return (
+//     <div class="project-entry d-md-flex flex-md-row mb-3">
+//     <div class="project-header">
+//       <img
+//         src={project.preview}
+//         class="project-img"
+//         style={{minHeight:"180px", minWidth:"200px"}}
+//       />
+//     </div>
+//     <div class="project-body d-flex flex-column ms-md-2">
+//       <Link to={project.link}>
+//       <div class="project-title">{project.title}</div>
+//       </Link>
+//       <div class="project-text">
+//       {project.text}
+//       </div>
+//       <div class="project-tag">
+//         {
+//             project.skills.forEach((skill)=>{
+//                 return (
+//                     <div class="skill-tag"> {skill} </div>
+//                 )
+//             })
+//         }
+//       </div>
+//     </div>
+
+//   </div>
+//   )
+// }
 
 export default ProjectEntry;
